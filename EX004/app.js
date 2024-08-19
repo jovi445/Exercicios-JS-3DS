551b0bcd9b1f003c9c23ce1dbfe39a64e8a46994
João Vitor <borgesjoaovitor444@gmail.com>
2024-08-19 08:30:34 -0300

    Commit

---
+++ EX004/app.js
@@ -0,0 +1,5 @@
+console.log("Teste");
+const idade =18
+console.log(idade + 3)
+console.log(idade / 2)
+console.log(idade * 2)
---
+++ EX005/app.js
@@ -0,0 +1,7 @@
+const num = 7
+
+if (num > 9) {
+    console.log(num, "> 9")
+} else {
+    console.log(num, "< 9")
+}
